2d4efaa07935c5e02c05470eed578faf962fa057
Nadeem Ahmed <aknadeemahmed@gmail.com>
2024-07-27 23:36:26 +0500

    resolving pipelines

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,16 +8,16 @@ export const Footer = () => {
                 </span>
                 <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                     <li>
-                        <a href="#" target="_blank" className="hover:underline me-4 md:me-6">Instagram</a>
+                        <a href="/" target="_blank" className="hover:underline me-4 md:me-6">Instagram</a>
                     </li>
                     <li>
-                        <a href="#" target="_blank" className="hover:underline me-4 md:me-6">LinkedIn</a>
+                        <a href="/" target="_blank" className="hover:underline me-4 md:me-6">LinkedIn</a>
                     </li>
                     <li>
-                        <a href="#" target="_blank" className="hover:underline me-4 md:me-6">YouTube</a>
+                        <a href="/" target="_blank" className="hover:underline me-4 md:me-6">YouTube</a>
                     </li>
                     <li>
-                        <a href="#" target="_blank" className="hover:underline">Github</a>
+                        <a href="/" target="_blank" className="hover:underline">Github</a>
                     </li>
                 </ul>
             </div>
